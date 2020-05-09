603adb48b3994b59f894eaebdfefc64fabfbed81
vizthex123 <vizthex123@gmail.com>
2020-05-08 22:11:35 -0500

    Add files via upload

--- Items/Weapons/SoulSword_Cursed.cs
+++ Items/Weapons/SoulSword_Cursed.cs
@@ -4,19 +4,19 @@ using Terraria.ModLoader;
 
 namespace UpgradableGear.Items.Weapons
 {
-	public class SoulSword_Ichor : ModItem
+	public class SoulSword_Cursed : ModItem
 	{
 		public override void SetStaticDefaults() 
 		{
-			DisplayName.SetDefault("Ichor-Coated Soul Sword");
-			Tooltip.SetDefault("You've transformed the sword into a more specialized one.\nDoes less damage, but reduces enemy defense by 20\nCurrent Tier: Ichor-X");
+			DisplayName.SetDefault("Cursed Soul Sword");
+			Tooltip.SetDefault("You've transformed the sword into a more specialized one.\nEnemies no longer drop extra money, Knockback is slightly reduced, Frostburn is replaced with Cursed Inferno\nCursed Inferno is inflicted 100% of the time\nCurrent Tier: Cursed-X");
 	    }
 
 		public override void SetDefaults() 
 		{
-			item.damage = 200;
+			item.damage = 215;
 			item.knockBack = 8;
-			item.crit = 14;
+			item.crit = 16;
 			item.melee = true;
 			item.width = 70;
 			item.height = 85;
@@ -35,8 +35,8 @@ namespace UpgradableGear.Items.Weapons
 			
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(mod.GetItem("SoulSword_T10"), 1);
-			recipe.AddIngredient(ItemID.FlaskofIchor, 1);
-			recipe.AddIngredient(ItemID.Ichor, 3);
+			recipe.AddIngredient(ItemID.FlaskofCursedFlames, 1);
+			recipe.AddIngredient(ItemID.CursedFlames, 3);
 			recipe.AddIngredient(mod.GetItem("MobSoul"), 150);
 			//recipe.AddTile(TileType<SoulInfuser>());
 			recipe.AddTile(TileID.WorkBenches);
@@ -52,7 +52,7 @@ namespace UpgradableGear.Items.Weapons
 			player.HealEffect(healingAmount, true);
 			
 			if(Main.rand.NextBool(1)){
-			target.AddBuff(BuffID.Ichor, 300);
+			target.AddBuff(BuffID.CursedInferno, 420);
 		}
 		}
 	}
