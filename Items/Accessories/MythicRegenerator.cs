25f8dac531a90fac7f6a841e36a61a40f8215a80
vizthex123 <vizthex123@gmail.com>
2020-05-10 13:53:44 -0500

    Partially disabled

--- Items/Accessories/MythicRegenerator.cs
+++ Items/Accessories/MythicRegenerator.cs
@@ -9,7 +9,7 @@ namespace SoulSang.Items.Accessories
 		public override void SetStaticDefaults() 
 		{
 			DisplayName.SetDefault("Mythic Regenerator");
-			Tooltip.SetDefault("An ancient, legendary item made from 3 relics of the past\nImproves life regeneration and maximum health\nReduces Potion Sickness to 40 seconds\nCurrently BROKEN!");
+			Tooltip.SetDefault("An ancient, legendary item made from 3 relics of the past\nImproves life regeneration and maximum health\nReduces Potion Sickness to 40 seconds (currently not applied due to bugs)");
 		}
 		
         public override void SetDefaults()
@@ -26,19 +26,18 @@ namespace SoulSang.Items.Accessories
 			// Band of Regen is 1
 			player.lifeRegen+= 6;
 			player.statLifeMax2+= 100;
-			player.potionDelay = 40;
+			player.potionDelayTime+= 2400;
         }
         public override void AddRecipes()
         {
-			/*
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(mod.GetItem("LifeRegenerator"), 1);
 			recipe.AddIngredient(ItemID.CharmofMyths, 1);
 			recipe.AddTile(TileID.TinkerersWorkbench);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
-			*/
         }
     }
 }
 // Thanks to absoluteAquarian#5189 on the TML Discord for giving me the potion delay code
+// Thanks to Trivaxy#6867 for helping me fix the potion delay code
