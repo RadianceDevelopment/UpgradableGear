5af9c5194fd7e2907d184b4bb4681c1afdc81db5
vizthex123 <vizthex123@gmail.com>
2020-05-11 01:18:28 -0500

    Added unused WIP code to try and make RecipeGroups work
    But since I'm stupid and don't know what I'm doing, it's not working

--- SoulSang.cs
+++ SoulSang.cs
@@ -1,11 +1,39 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 
 namespace SoulSang
 {
 	public class SoulSang : Mod
 	{
+		/*
 		public SoulSang()
 		{
 		}
+		
+		public override void AddRecipeGroups()
+	    {
+		RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Final Melee Weapon", new int[]
+		{
+			ItemID.Meowmere,
+			ItemID.StarWrath,
+			ItemID.Terrarian
+			
+		});
+		RecipeGroup.RegisterGroup("SoulSang:FinalMelee", group);
+	    }
+	 
+	    public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(this);
+			recipe.AddRecipeGroup("SoulSang:FinalMelee");
+			recipe.AddIngredient(ModContent.ItemType<Items.Accessories.CombEssence>);
+			//recipe.AddIngredient(mod.GetItem("CombEssence"), 1);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(ModContent.ItemType<Items.Accessories.TrueCombEssence>());
+			recipe.AddRecipe();
+		}	
+		*/
 	}
 }
