e01e081b0263f7c5722a1c61a8006ae381002064
vizthex123 <vizthex123@gmail.com>
2020-05-14 00:05:43 -0500

    Add files via upload

---
+++ Items/Potions/SoulskinPotion.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SoulSang.Items.Potions
+{
+	public class SoulskinPotion : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Soulskin Potion");
+            Tooltip.SetDefault("Increases defense by 12");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 25;
+            item.useStyle = ItemUseStyleID.EatingUsing;
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.useTurn = true;
+            item.UseSound = SoundID.Item3;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.rare = 1;
+			item.value = Item.sellPrice(0, 0, 2, 15);
+            item.buffType = BuffType<Buffs.Soulskin>();
+            item.buffTime = 18000; //5 Mins
+		}	
+	    
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.GetItem("MobSoul"), 5);
+			recipe.AddIngredient(ItemID.IronskinPotion);
+			recipe.AddIngredient(ItemID.SoulofNight);
+			recipe.AddTile(TileID.Bottles);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.GetItem("MobSoul"), 5);
+			recipe.AddIngredient(ItemID.IronskinPotion);
+			recipe.AddIngredient(ItemID.SoulofLight);
+			recipe.AddTile(TileID.Bottles);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
