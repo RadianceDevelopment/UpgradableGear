3ac0463b1ede9059b1a1d0c1b6424e764d040030
vizthex123 <vizthex123@gmail.com>
2020-05-14 00:04:54 -0500

    Add files via upload

---
+++ Buffs/Soulskin.cs
@@ -0,0 +1,21 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SoulSang.Buffs
+{
+    public class Soulskin : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Soulskin");
+            Description.SetDefault("Grants +12 defense.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += 12;
+        }
+    }
+}
