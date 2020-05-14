91d99cdb8244e35cde7c4acb04fb3a42f2d35742
vizthex123 <vizthex123@gmail.com>
2020-05-14 00:21:17 -0500

    Add files via upload

---
+++ Items/Potions/SoulspeedPotion.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SoulSang.Items.Potions
+{
+	public class SoulspeedPotion : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Soulspeed Potion");
+            Tooltip.SetDefault("Increases movement speed by 40%");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 25;
+            item.useStyle = ItemUseStyleID.EatingUsing;
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.useTurn = true;
+            item.UseSound = SoundID.Item3;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.rare = 1;
+			item.value = Item.sellPrice(0, 0, 4, 4);
+            item.buffType = BuffType<Buffs.Soulspeed>();
+            item.buffTime = 14400; //4 Mins
+		}	
+	    
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.GetItem("MobSoul"), 5);
+			recipe.AddIngredient(ItemID.SwiftnessPotion);
+			recipe.AddIngredient(ItemID.SoulofNight);
+			recipe.AddTile(TileID.Bottles);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.GetItem("MobSoul"), 5);
+			recipe.AddIngredient(ItemID.SwiftnessPotion);
+			recipe.AddIngredient(ItemID.SoulofLight);
+			recipe.AddTile(TileID.Bottles);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
