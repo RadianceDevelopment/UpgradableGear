24857f361564bba4f7998e63463ac56784e8e6b7
vizthex123 <vizthex123@gmail.com>
2020-05-15 21:47:37 -0500

    Basic code laid down, projectile still NYI

---
+++ Projectiles/GhostOrb.cs
@@ -0,0 +1,31 @@
+/*
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SoulSang.Projectiles
+{
+	public class GhostOrb : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ghostly Orb");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.alpha = 150;
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.aiStyle = 116; //Should be a sword beam
+			projectile.friendly = true;
+			projectile.melee = true;
+			//aiType = ProjectileID.WoodenArrowFriendly;
+			//I apparently don't need this?
+			//projectile.damage = 30;
+			projectile.maxPenetrate = 1;
+		}
+	}
+}
+*/
+// Todo: Make this actually fire from high-tier {7+} Soul Swords, finish the code
